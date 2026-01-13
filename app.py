f2270fd1d76177c1cc6e8b3b4ce41848ff05f3fb
Arti Gupta <artigupta.gupta68@gmail.com>
2026-01-13 19:21:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import json
 # -----------------------------
 # 1️⃣ Configuration
 # -----------------------------
-DRIVE_FILE_ID = "DRIVE_FILE_ID = "1S6fcsuq9KvICTsOBOdp6_WN9FhzruixM"  # syllabus ZIP
+DRIVE_FILE_ID = "1S6fcsuq9KvICTsOBOdp6_WN9FhzruixM"  # syllabus ZIP
 "  # syllabus ZIP
 LOCAL_ZIP = "plan.zip"
 EXTRACT_DIR = "syllabus_data"
