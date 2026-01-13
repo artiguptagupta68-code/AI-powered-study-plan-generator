a03c833f27a408d30aeea5dff52a78f85fb8d34a
Arti Gupta <artigupta.gupta68@gmail.com>
2026-01-13 21:10:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,18 +65,18 @@ def detect_exam_stage(pdf_path, lines):
         return "IIT JEE", stage
 
    # ---------------- GATE ----------------
-   if "GATE" in folder or "GATE" in filename or "GRADUATE APTITUDE TEST" in text:
-       branch = None
+    if "GATE" in folder or "GATE" in filename or "GRADUATE APTITUDE TEST" in text:
+        branch = None
     # Scan the entire PDF for branch
-       for l in lines:
-           l_clean = l.strip()
+        for l in lines:
+            l_clean = l.strip()
         # Branch is usually uppercase, short (<=5 words), not the word 'GATE', not a number
-           if l_clean.isupper() and len(l_clean.split()) <= 5 and "GATE" not in l_clean and not l_clean.isdigit():
-               branch = l_clean
-               break
-   if not branch:
-       branch = "General"  # fallback if branch not found
-       return "GATE", branch
+            if l_clean.isupper() and len(l_clean.split()) <= 5 and "GATE" not in l_clean and not l_clean.isdigit():
+                branch = l_clean
+                break
+     if not branch:
+        branch = "General"  # fallback if branch not found
+        return "GATE", branch
 
 
     return "UNKNOWN", "General"
